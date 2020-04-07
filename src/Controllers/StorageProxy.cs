5df637f95bfe8c271bf968df6bffbe666a6619ec
Yuuki Wesp <ls-micro@ya.ru>
2020-04-07 06:08:40 +0300

    add main controller

---
+++ src/Controllers/StorageProxy.cs
@@ -0,0 +1,34 @@
+﻿namespace ruler.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Features;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Octokit;
+
+    [ApiController]
+    public class StorageProxy : ControllerBase
+    {
+        private readonly GithubAdapter _adapter;
+
+        public StorageProxy(GithubAdapter adapter) => _adapter = adapter;
+
+        [HttpPost("/api/storage")]
+        public async Task<IActionResult> AddPackageAsync([FromBody] IFormFile file, CancellationToken cancellationToken = default)
+        {
+            await using var memory = new MemoryStream();
+            await file.CopyToAsync(memory, cancellationToken);
+            
+
+            var commit = await _adapter.CreateCommitAsync("packages/test.nuget", memory, cancellationToken);
+
+            await _adapter.Push(commit);
+
+            return StatusCode(200);
+        }
+    }
+}
