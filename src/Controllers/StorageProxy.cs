6e8aa83db89451a39cac0206eb5c68705391924c
Yuuki Wesp <ls-micro@ya.ru>
2020-04-07 07:05:45 +0300

    fix action

--- src/Controllers/StorageProxy.cs
+++ src/Controllers/StorageProxy.cs
@@ -1,8 +1,9 @@
-﻿namespace ruler.Controllers
+namespace ruler.Controllers
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Features;
@@ -14,17 +15,24 @@
     public class StorageProxy : ControllerBase
     {
         private readonly GithubAdapter _adapter;
+        private readonly CancellationToken _cancellationToken;
 
-        public StorageProxy(GithubAdapter adapter) => _adapter = adapter;
+        public StorageProxy(GithubAdapter adapter, CancellationToken cancellationToken = default)
+        {
+            _adapter = adapter;
+            _cancellationToken = cancellationToken;
+        }
 
         [HttpPost("/api/storage")]
-        public async Task<IActionResult> AddPackageAsync([FromBody] IFormFile file, CancellationToken cancellationToken = default)
+        public async Task<IActionResult> AddPackageAsync()
         {
+            var files = Request.Form.Files;
+            var file = files.First();
             await using var memory = new MemoryStream();
-            await file.CopyToAsync(memory, cancellationToken);
+            await file.CopyToAsync(memory, _cancellationToken);
             
 
-            var commit = await _adapter.CreateCommitAsync("packages/test.nuget", memory, cancellationToken);
+            var commit = await _adapter.CreateCommitAsync("packages/test.nuget", memory, _cancellationToken);
 
             await _adapter.Push(commit);
 
