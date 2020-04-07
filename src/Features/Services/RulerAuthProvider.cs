25db545196b68742e7c7b9687a26fb0c0e4e74b4
Yuuki Wesp <ls-micro@ya.ru>
2020-04-07 06:09:32 +0300

    add auth providers

---
+++ src/Features/Services/RulerAuthProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace ruler.Features
+{
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class RulerAuthProvider : IAuthProvider
+    {
+        private const string AuthFileName = "rune.credentials";
+        private const string Target = "api.v4.rune.local:567";
+        private readonly IFireStoreAdapter _adapter;
+
+        public RulerAuthProvider(IFireStoreAdapter adapter) => _adapter = adapter;
+
+
+        public async Task<string> GetTokenAsync(CancellationToken cancellationToken = default)
+        {
+            if (File.Exists($"./{AuthFileName}"))
+                return await File.ReadAllTextAsync($"./{AuthFileName}", cancellationToken);
+            var doc = _adapter.Cluster.Document("credentials");
+            var snap = await doc.GetSnapshotAsync(cancellationToken);
+            var token = snap.GetValue<string>(Target);
+
+            await File.WriteAllTextAsync($"./{AuthFileName}", token, cancellationToken);
+            return token;
+        }
+    }
+
+    public interface IAuthProvider
+    {
+        Task<string> GetTokenAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Features/Services/RulerExternalApiProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace ruler.Features
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Flurl.Http;
+
+    public class RulerExternalApiProvider : IRulerAPI
+    {
+        private const string origin = "https://cluster.ruler.runic.cloud";
+        private readonly IFireStoreAdapter _adapter;
+        private readonly IAuthProvider _authProvider;
+
+        public RulerExternalApiProvider(IFireStoreAdapter adapter, IAuthProvider authProvider)
+        {
+            _adapter = adapter;
+            _authProvider = authProvider;
+        }
+
+
+
+        public async Task<string> GetGithubToken(CancellationToken cancellationToken = default)
+        {
+            var token = await _authProvider.GetTokenAsync(cancellationToken);
+            return await $"{origin}/@me/container/github"
+                .WithHeader("User-Agent", "api.ruler/cloud")
+                .WithOAuthBearerToken(token)
+                .GetStringAsync(cancellationToken);
+        }
+    }
+
+    public interface IRulerAPI
+    {
+        Task<string> GetGithubToken(CancellationToken cancellationToken = default);
+    }
+}
