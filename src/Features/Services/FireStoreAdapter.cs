7a1ab4eb77a4f15c922b41c5b100ec9402e88cab
Yuuki Wesp <ls-micro@ya.ru>
2020-04-07 06:09:03 +0300

    add filestore adapter

---
+++ src/Features/Services/FireStoreAdapter.cs
@@ -0,0 +1,18 @@
+﻿namespace ruler.Features
+{
+    using Google.Cloud.Firestore;
+
+    public class FireStoreAdapter : IFireStoreAdapter
+    {
+        private FirestoreDb db { get; }
+        public FireStoreAdapter() => db = FirestoreDb.Create();
+
+
+        public CollectionReference Cluster => db.Collection("cluster");
+    }
+
+    public interface IFireStoreAdapter
+    {
+        CollectionReference Cluster { get; }
+    }
+}
