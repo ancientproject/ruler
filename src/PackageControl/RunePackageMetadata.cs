d0c397fe7cacd49d2fb4980bd79fee1c1087cafc
Yuuki Wesp <ls-micro@ya.ru>
2020-04-10 11:25:01 +0300

    add metadata of pacakge

---
+++ src/PackageControl/RunePackageMetadata.cs
@@ -0,0 +1,15 @@
+﻿namespace ruler.Features
+{
+    using Google.Cloud.Firestore;
+
+    [FirestoreData]
+    public class RunePackageMetadata
+    {
+        public RunePackageMetadata() { }
+
+        [FirestoreProperty]
+        public MetadataStatusType Status { get; set; }
+        [FirestoreProperty]
+        public ulong DownloadCount { get; set; }
+    }
+}
