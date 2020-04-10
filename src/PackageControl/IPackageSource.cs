64f9fd97bd1b9dcd07e4bc08478042ec66258b80
Yuuki Wesp <ls-micro@ya.ru>
2020-04-10 11:25:50 +0300

    add interface of package source

---
+++ src/PackageControl/IPackageSource.cs
@@ -0,0 +1,14 @@
+﻿namespace ruler.Features
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NuGet.Versioning;
+
+    public interface IPackageSource
+    {
+        Task New([DisallowNull] RunePackage package);
+        Task Delete([DisallowNull] string ID, [DisallowNull] NuGetVersion version, [DisallowNull] string reason);
+        Task<RunePackage> Get(string ID, NuGetVersion version = null, CancellationToken cancellationToken = default);
+    }
+}
